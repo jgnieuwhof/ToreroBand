561bbda36e7ddab19faebf4550292655f9742c02
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2017-04-18 08:05:00 -0400

    no need for shopify yet, copy pasta fail

--- app/index.js
+++ app/index.js
@@ -6,29 +6,19 @@ import './index.scss'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, browserHistory } from 'react-router'
-import ShopifyBuy from 'shopify-buy'
 
-import appConfig from './app.config'
 import Routes from './components/Routes'
-import ShopifyProvider from './components/ShopifyProvider'
-
-const SHOPIFY = {
-  client: ShopifyBuy.buildClient(appConfig.shopify),
-  cart: {},
-}
 
 window.onload = () => {
   let root = document.createElement(`div`)
   document.body.appendChild(root)
   ReactDOM.render(
-    <ShopifyProvider shopify={SHOPIFY}>
-      <Router
-        onUpdate={() => window.scrollTo(0, 0)}
-        history={browserHistory}
-      >
-        { Routes }
-      </Router>
-    </ShopifyProvider>,
+    <Router
+      onUpdate={() => window.scrollTo(0, 0)}
+      history={browserHistory}
+    >
+      { Routes }
+    </Router>,
     root
   )
 }
